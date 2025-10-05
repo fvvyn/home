69d1adb5b6e44d2dd268308cc1b3d48aa1fb3ee7
fvvyn <fvvyn.brand@gmail.com>
2025-10-05 23:44:33 +0900

    Create products.js

---
+++ products.js
@@ -0,0 +1,7 @@
+// products.js
+
+export const VALID_PRODUCTS = [
+  { id: "1A2B3C4D5E6F", name: "fvvyn official products" },
+  { id: "9876543210ZY", name: "Tshirts" },
+  { id: "FGHJ5678WERT", name: "1.α" },
+];
